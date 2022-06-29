6e1eaf2e320eab44993be8a06069066ae672880f
D3KRYPT <92589899+D3KRYPT@users.noreply.github.com>
2022-06-30 00:28:54 +0530

    Time: 4 ms (63.44%), Space: 6.7 MB (60.41%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -1,23 +1,40 @@
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
-        vector<int>last(26);
-        vector<bool>vis(26);
-        string ans = "";
-        
+        vector<int> last(26);
         int n = s.size();
-        for(int i = 0; i < n; i++) last[s[i]-'a'] = i;        
         
-        for(int i = 0; i < n; i++){
-            if(vis[s[i] - 'a']) continue;
- 
-            while(!ans.empty() && ans.back() > s[i] && last[ans.back()-'a'] > i){
-                vis[ans.back() - 'a'] = false;      
-                ans.pop_back();
-            }
-            ans.push_back(s[i]);      
-            vis[s[i] - 'a'] = true; 
+        for(int i = 0; i < n; i++)
+        {
+            last[s[i] - 'a'] = i;
         }
+        
+        vector<bool> seen(26, false);
+        stack <char> st;
+        
+        for(int i = 0; i < n; i++)
+        {
+            if(seen[s[i] - 'a'])
+                continue;
+            //if the element is not taken, only then we take it, otherwise we skip the element.
+            while(st.size() > 0 && st.top() > s[i] && last[st.top() - 'a'] > i)
+            {
+                seen[st.top() - 'a'] = false;
+                st.pop(); 
+            }
+            
+            st.push(s[i]);
+            seen[s[i] - 'a'] = true;
+        }
+        
+        string ans = "";
+        while(!st.empty())
+        {
+            ans = ans + st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),  ans.end());
+        
         return ans;
     }
 };
