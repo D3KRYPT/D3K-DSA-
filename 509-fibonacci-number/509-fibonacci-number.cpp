a4fd194eceaedb56757cb2368087fbe6ee2b8ce4
D3KRYPT <92589899+D3KRYPT@users.noreply.github.com>
2022-07-10 23:33:50 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (16.56%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int fib(int n) {
         //vector<int> dp(31, -1);
-       static vector<int> dp(31+n, -1);
+       static vector<int> dp(n+31, -1);
        if(n <= 1) return n;
         
         if(dp[n] != -1) 
