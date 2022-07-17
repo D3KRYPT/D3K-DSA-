60174f7ea85c17c224fa0454aa0e8aab223b10cd
D3KRYPT <92589899+D3KRYPT@users.noreply.github.com>
2022-07-17 19:06:05 +0530

    Time: 436 ms (6.33%), Space: 73.1 MB (25.52%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
-    int findLCS(string &a, string &t, int i, int j, vector<vector<int>> &dp)
-  {
-      if(i < 0 || j < 0) return 0;
-      
-      if(dp[i][j] != -1) return dp[i][j];
-      if(a[i] == t[j]) return dp[i][j] =  1 + findLCS(a, t, i - 1, j - 1, dp);
-      return dp[i][j] = max(findLCS(a, t, i, j - 1, dp), findLCS(a, t, i - 1, j, dp));
-  }
-    int longestPalindromeSubseq(string s) {
+    int longestPalindromeSubseq(string s)
+    {
         int a = s.length();
-        vector<vector<int>> dp(a, vector<int> (a, -1));
+        vector<vector<int>> dp(a+1, vector<int> (a+1, 0));
         string t = s;
         reverse(t.begin(), t.end());
         //cout << t;
-        return findLCS(s, t, a - 1, a - 1, dp);
+        
+        int langthMax = 0;
+        for(int i = 1; i <= a; i++)
+        {
+            for(int j = 1; j <= a; j++)
+            {
+            if(s[i - 1] == t[j - 1]) 
+            {
+                dp[i][j] =  1 + dp[i - 1][j - 1];
+            }
+                
+              else dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);   
+            }
+        }
+        
+        return dp[a][a];
     }
 };
