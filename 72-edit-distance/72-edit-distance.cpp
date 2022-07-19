28bbcafb0a6f3728fd4fcad53943700f02aec932
D3KRYPT <92589899+D3KRYPT@users.noreply.github.com>
2022-07-19 18:38:52 +0530

    Time: 28 ms (36.82%), Space: 9 MB (38.38%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,22 +1,25 @@
 class Solution {
-private:
-    int find(int n, int m, string &s, string &t, vector<vector<int>> &dp)
-    {
-        if(n == 0) return m ;
-        if(m == 0) return n;
-        if(dp[n][m] != -1) return dp[n][m];
-        
-        if(s[n-1] == t[m-1]) return dp[n][m] = 0 + find(n - 1, m - 1, s, t, dp);
-        return dp[n][m] = 1 +  min(find(n, m - 1, s, t, dp), min (find(n - 1, m, s, t, dp), find(n - 1, m - 1, s, t, dp)));
-    }
 public:
     int minDistance(string s, string t) {
         
         int n = s.length();
         int m = t.length();
         vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
-        return find(n, m , s, t, dp);
         
+        for(int i = 0; i <= n; i++) dp[i][0] = i;
+        for(int j = 0; j <= m; j++) dp[0][j] = j;
         
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= m; j++)
+            {
+              
+                if(s[i-1] == t[j-1]) dp[i][j] = 0 + dp[i - 1][j - 1];
+                
+                else
+                    dp[i][j] = 1 +  min( dp[i][j - 1],min(dp[i - 1][j], dp[i - 1][j - 1]));  
+            }
+        }
+        return dp[n][m];  
     }
 };
