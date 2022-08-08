aed4b8b5791d8c86891648b9e4159af3744cad92
D3KRYPT <92589899+D3KRYPT@users.noreply.github.com>
2022-08-08 19:44:25 +0530

    Time: 16 ms (91.63%), Space: 16.5 MB (96.73%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,22 +1,22 @@
 class Solution {
-    private:
-    int find(int ind, vector<int> &nums, int n, vector<int> &dp)
-    {
-        if(ind >= n - 1) return 0;
-        
-        int temp = 1e9;
-        
-        for(int i = 1; i <= nums[ind]; i++)
-        {
-            if(dp[ind] != -1) return dp[ind];
-            temp = min(temp, 1 + find(ind + i, nums, n, dp));
-        }
-        return dp[ind] = temp;
-    }
 public:
     int jump(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n, -1);
-        return find(0, nums, n, dp);
+        int n  = nums.size();
+        
+        int farthest = 0;
+        int curr = 0;
+        int jumps = 0;
+        
+        for(int i = 0; i < nums.size() - 1; i++)
+        {
+            farthest = max(farthest, i + nums[i]);
+            
+            if(curr == i)
+            {
+                curr = farthest;
+                jumps++;
+            }
+        }
+        return jumps;
     }
 };
