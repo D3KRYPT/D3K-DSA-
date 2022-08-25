0b3904ea0215b48beb261785913b8fdf3e4e83d9
D3KRYPT <92589899+D3KRYPT@users.noreply.github.com>
2022-08-26 01:30:16 +0530

    Time: 34 ms (9.81%), Space: 15.7 MB (6.00%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -7,8 +7,10 @@ public:
             int hi = N;
             while(lo<hi){
                 int mid = lo + (hi-lo)/2;
-                if(nums[mid]==target-nums[i]) return {i+1,mid+1};
-                else if(nums[mid]>=target-nums[i]) hi=mid;
+                if(nums[mid]==target-nums[i]) 
+                    return {i+1,mid+1};
+                else if(nums[mid] > target-nums[i])
+                    hi=mid - 1;
                 else lo = mid+1;
             }
             if(lo!=nums.size() && nums[lo]==target-nums[i]) return {i+1,lo+1};
