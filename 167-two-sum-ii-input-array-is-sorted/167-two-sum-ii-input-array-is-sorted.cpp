c44a3268ecbb59ffe75563eb9dc44ef304e58944
D3KRYPT <92589899+D3KRYPT@users.noreply.github.com>
2022-08-26 01:29:09 +0530

    Time: 42 ms (5.82%), Space: 15.6 MB (44.30%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
-        int i = 0, j = numbers.size() - 1;
-        
-        while(i < j)
-        {
-            if(numbers[i] + numbers[j] == target)
-                return {i+1, j+1};
-            
-            else if(numbers[i] + numbers[j] < target)
-                i++;
-            else
-                j--;
+    vector<int> twoSum(vector<int>& nums, int target) {
+          int N = nums.size();
+        for(int i=0;i<N-1;i++){
+            int lo = i+1;
+            int hi = N;
+            while(lo<hi){
+                int mid = lo + (hi-lo)/2;
+                if(nums[mid]==target-nums[i]) return {i+1,mid+1};
+                else if(nums[mid]>=target-nums[i]) hi=mid;
+                else lo = mid+1;
+            }
+            if(lo!=nums.size() && nums[lo]==target-nums[i]) return {i+1,lo+1};
         }
-        
-        return {};
+        return {}; // not found
     }
 };
