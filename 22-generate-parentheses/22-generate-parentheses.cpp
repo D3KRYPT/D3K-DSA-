ad582650cfb14c6325531e9bc4c20024de6db630
D3KRYPT <92589899+D3KRYPT@users.noreply.github.com>
2022-09-02 08:31:06 +0530

    Time: 11 ms (28.87%), Space: 15.7 MB (19.28%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void solve(int open, int close, string op, vector <string> &res)
+    {
+        if(open == 0 && close == 0)
+        {
+            res.push_back(op);
+            return;
+        }
+        
+        if(open != 0)
+        {
+            string op1 = op;
+            op1.push_back('(');
+            solve(open - 1, close, op1, res);
+        }
+        
+        if(close > open)
+        {
+            string op2 = op;
+            op2.push_back(')');
+            solve(open, close - 1, op2, res);
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        // int open = 0, close = 0;
+        vector<string> res;
+        solve(n, n, "", res);
+        return res;
+    }
+};
