c965d27a15a33f6c7a52c946f278f8d475d0bdd6
D3KRYPT <92589899+D3KRYPT@users.noreply.github.com>
2022-10-27 23:15:20 +0530

    Time: 8 ms (11.33%), Space: 8.2 MB (90.77%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        int zeros = 0, ones = 0, twos = 0;
+        int n = nums.size();
+        for(int i = 0; i < n; i++)
+        {
+            if(nums[i] == 0)
+                zeros++;
+            else if(nums[i] == 1)
+                ones++;
+            else
+                twos++;
+        }
+        cout << zeros <<" "<<ones<<" "<<twos;
+        for(int i = 0; i < zeros; i++)
+        {
+            nums[i] = 0;
+        }
+        
+        for(int i = zeros; i < n - twos; i++)
+        {
+            nums[i] = 1;
+        }
+        
+        for(int i = zeros + ones; i < n; i++)
+        {
+            nums[i] = 2;
+        }
     }
 };
